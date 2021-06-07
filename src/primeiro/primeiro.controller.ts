7ec103102cea57297e71bfd25643fb3686fe5174
leoportogtr86 <leoporto_studio@hotmail.com>
2021-06-07 13:22:09 -0300

    primeira resposta na rota

--- src/primeiro/primeiro.controller.ts
+++ src/primeiro/primeiro.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
-@Controller('primeiro')
+
+
+@Controller('/api/primeiro')
 export class PrimeiroController {
+
+
+    @Get()
+    async index(){
+
+        return {msg:"Rota 1 do primeiro controller. Hello Nest!"}
+    }
 
     
 }
