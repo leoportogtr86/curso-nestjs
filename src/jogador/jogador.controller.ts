37c8f53c9f0e16c65003d824d18214d38565cc1e
leoportogtr86 <leoporto_studio@hotmail.com>
2021-06-30 07:06:08 -0300

    instanciando um jogador e retornando na rota

--- src/jogador/jogador.controller.ts
+++ src/jogador/jogador.controller.ts
@@ -1,13 +1,28 @@
 import { Controller, Get } from '@nestjs/common';
+import {Jogador} from './interfaces/Jogador.interface'
+import {CriarJogadorDto} from './dto/Jogador.dto'
+
+
+let j1: Jogador = {
+
+    _id: '1',
+    telefone: '(12)123654879',
+    email: 'email@gmail.com',
+    nome: 'Fulano de Tahl',
+    ranking: 'liga master',
+    posicao: 3,
+    url_foto: 'www.123.com'
+
+}
 
 @Controller('jogador')
 export class JogadorController {
 
 
     @Get()
-    async index(){
+    async index(): Promise<Jogador>{
 
-        return {msg: 'Controller Jogador'}
+        return j1
 
 
     }
