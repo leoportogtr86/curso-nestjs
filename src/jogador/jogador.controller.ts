56d1bb230af5f545378f855c6a8e560254ba1258
leoportogtr86 <leoporto_studio@hotmail.com>
2021-06-30 07:25:47 -0300

    delegando o retorno da requisicao de dados ao service e injetando-o no controller

--- src/jogador/jogador.controller.ts
+++ src/jogador/jogador.controller.ts
@@ -1,28 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
+
 import {Jogador} from './interfaces/Jogador.interface'
-import {CriarJogadorDto} from './dto/Jogador.dto'
+import {JogadorService} from './jogador.service'
 
 
-let j1: Jogador = {
-
-    _id: '1',
-    telefone: '(12)123654879',
-    email: 'email@gmail.com',
-    nome: 'Fulano de Tahl',
-    ranking: 'liga master',
-    posicao: 3,
-    url_foto: 'www.123.com'
-
-}
 
 @Controller('jogador')
 export class JogadorController {
+
+    constructor(private jogadorService: JogadorService){
+
+    }
+
+
 
 
     @Get()
     async index(): Promise<Jogador>{
 
-        return j1
+        return this.jogadorService.getJogador()
 
 
     }
--- src/jogador/jogador.service.ts
+++ src/jogador/jogador.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
 
+import {Jogador} from './interfaces/Jogador.interface'
+
+
+let j1: Jogador = {
+
+    _id: '1',
+    telefone: '(12)123654879',
+    email: 'email@gmail.com',
+    nome: 'Fulano de Tahl',
+    ranking: 'liga master',
+    posicao: 3,
+    url_foto: 'www.123.com'
+
+}
+
 @Injectable()
-export class JogadorService {}
+export class JogadorService {
+
+
+    async getJogador(): Promise<Jogador>{
+
+        return j1
+
+    }
+}
